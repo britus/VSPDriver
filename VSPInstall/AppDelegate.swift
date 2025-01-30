4af911e0099ca694fa4e66933d315dd045a4f7ff
B. Eschrich <bjoern.eschrich@gmail.com>
2025-01-30 14:10:29 +0100

    wip

--- VSPInstall/AppDelegate.swift
+++ VSPInstall/AppDelegate.swift
@@ -9,13 +9,16 @@ import Cocoa
 import SystemExtensions
 
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
+class AppDelegate: NSObject, NSApplicationDelegate, OSSystemExtensionRequestDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         let request = OSSystemExtensionRequest.activationRequest(
-            forExtensionWithIdentifier: "org.eof.tools.VSPDriver", queue: .main
+              forExtensionWithIdentifier: "com.example.VirtualSerialPortDriver",
+              queue: .main
         )
         request.delegate = self
-  
         OSSystemExtensionManager.shared.submitRequest(request)
     }
 
@@ -26,24 +29,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-}
-
-extension AppDelegate: OSSystemExtensionRequestDelegate {
+    
     func request(_ request: OSSystemExtensionRequest, actionForReplacingExtension existing: OSSystemExtensionProperties, withExtension ext: OSSystemExtensionProperties) -> OSSystemExtensionRequest.ReplacementAction {
-        return .replace
+      return .replace
     }
 
     func requestNeedsUserApproval(_ request: OSSystemExtensionRequest) {
-        print("User approval required.")
+      print("User approval required.")
     }
 
     func request(_ request: OSSystemExtensionRequest, didFinishWithResult result: OSSystemExtensionRequest.Result) {
-        print("Activation succeeded.")
-        NSApplication.shared.terminate(nil)
+      print("Activation succeeded.")
+      NSApplication.shared.terminate(nil)
     }
 
     func request(_ request: OSSystemExtensionRequest, didFailWithError error: Error) {
-        print("Activation failed: \(error)")
-        NSApplication.shared.terminate(nil)
+      print("Activation failed: \(error)")
+      NSApplication.shared.terminate(nil)
     }
 }
+
