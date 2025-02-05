545a585bb6aeda71092a37e329f6eb7c8b477acd
B. Eschrich <bjoern.eschrich@gmail.com>
2025-02-05 18:47:16 +0100

    added

---
+++ vspshow.sh
@@ -0,0 +1,2 @@
+# sudo log show -style compact --color always --info --debug --backtrace --source --process 0 | egrep 'IOUserSerial|VSP|VCP'
+sudo log show -style compact --color always --info --debug --backtrace --process 0 | egrep 'IOUserSerial|VSP|VCP'
