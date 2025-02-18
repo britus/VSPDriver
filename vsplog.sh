15d781bee0215065f3d724c78ae08643b372b6ec
B. Eschrich (PARI-PACC) <bjoern.eschrich@pari.com>
2025-02-18 09:43:00 +0100

    obsolete stuff removed

--- vsplog.sh
+++
@@ -1,3 +0,0 @@
-sudo log erase ; sleep 2 ; \
-sudo log stream --style compact --color always \
-	--backtrace --debug --info --process 0 | egrep 'error|timeout|VSP|IOUser'
--- vspshow.sh
+++
@@ -1,2 +0,0 @@
-# sudo log show -style compact --color always --info --debug --backtrace --source --process 0 | egrep 'IOUserSerial|VSP|VCP'
-sudo log show -style compact --color always --info --debug --backtrace --process 0 | egrep 'IOUserSerial|VSP|VCP'
