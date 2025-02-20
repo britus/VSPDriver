8db77980e1137083fb914575d80a4e77d7303e7e
B. Eschrich <bjoern.eschrich@gmail.com>
2025-02-20 10:39:36 +0100

    uc controller updated

--- VSPController/VSPController.hpp
+++ VSPController/VSPController.hpp
@@ -17,6 +17,37 @@ extern "C" {
 
 namespace VSPClient {
 
+#define MAGIC_CONTROL 0xBE6605250000L
+#define MAX_SERIAL_PORTS 16
+#define MAX_PORT_LINKS 8
+#define MAX_PORT_NAME 64
+
+#ifndef VSP_UCD_SIZE
+#define VSP_UCD_SIZE sizeof(TVSPControllerData)
+#endif
+
+typedef enum {
+    vspContextPing   = 0x01,
+    vspContextPort   = 0x02,
+    vspContextResult = 0x03,
+    vspContextError  = 0x04,
+} TVSPUserContext;
+
+typedef enum {
+    vspControlPingPong,
+    vspControlGetStatus,
+    vspControlCreatePort,
+    vspControlRemovePort,
+    vspControlLinkPorts,
+    vspControlUnlinkPorts,
+    vspControlGetPortList,
+    vspControlGetLinkList,
+    vspControlEnableChecks,
+    vspControlEnableTrace,
+    // Has to be last
+    vspLastCommand,
+} TVSPControlCommand;
+
 typedef struct {
     uint32_t baudRate;
     uint8_t dataBits;
@@ -24,6 +55,50 @@ typedef struct {
     uint8_t parity;
     uint8_t flowCtrl;
 } TVSPPortParameters;
+
+typedef struct {
+    uint8_t id;
+    char    name[MAX_PORT_NAME];
+} TVSPPortListItem;
+
+typedef struct {
+    /* In whitch context calld */
+    uint8_t context;
+    
+    /* User client command */
+    uint8_t command;
+    
+    /* Command status response */
+    struct Status {
+        uint32_t code;
+        uint64_t flags;
+    } status;
+    
+    /* Command parameters */
+    struct Parameter {
+        /* parameter flags */
+        uint64_t flags;
+       
+        /* port link parameters */
+        struct PortLink {
+            uint8_t source;
+            uint8_t target;
+        } link;
+    } parameter;
+    
+    /* Available serial ports */
+    struct PortList {
+        uint8_t count;
+        TVSPPortListItem list[MAX_SERIAL_PORTS];
+    } ports;
+    
+    /* Available serial port links */
+    struct LinkList {
+        uint8_t count;
+        uint64_t list[MAX_PORT_LINKS];
+    } links;
+    
+} TVSPControllerData;
 
 class VSPControllerPriv;
 
--- VSPController/VSPControllerPriv.hpp
+++ VSPController/VSPControllerPriv.hpp
@@ -10,93 +10,9 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <IOKit/IOKitLib.h>
 #include <IOKit/IOTypes.h>
-
-#define MAGIC_CONTROL 0xBE6605250000L
-#define MAX_SERIAL_PORTS 16
-#define MAX_PORT_LINKS 8
-#define MAX_PORT_NAME 64
-
-#ifndef VSP_UCD_SIZE
-#define VSP_UCD_SIZE sizeof(TVSPControllerData)
-#endif
-
-typedef enum {
-    vspContextPing   = 0x01,
-    vspContextPort   = 0x02,
-    vspContextResult = 0x03,
-    vspContextError  = 0x04,
-} TVSPUserContext;
-
-typedef enum {
-    vspControlPingPong,
-    vspControlGetStatus,
-    vspControlCreatePort,
-    vspControlRemovePort,
-    vspControlLinkPorts,
-    vspControlUnlinkPorts,
-    vspControlGetPortList,
-    vspControlGetLinkList,
-    vspControlEnableChecks,
-    vspControlEnableTrace,
-    // Has to be last
-    vspLastCommand,
-} TVSPControlCommand;
-
-typedef struct {
-    uint32_t baudRate;
-    uint8_t dataBits;
-    uint8_t stopBits;
-    uint8_t parity;
-    uint8_t flowCtrl;
-} TVSPPortParameters;
-
-typedef struct {
-    uint8_t id;
-    char    name[MAX_PORT_NAME];
-} TVSPPortListItem;
-
-typedef struct {
-    /* In whitch context calld */
-    uint8_t context;
-    
-    /* User client command */
-    uint8_t command;
-    
-    /* Command status response */
-    struct Status {
-        uint32_t code;
-        uint64_t flags;
-    } status;
-    
-    /* Command parameters */
-    struct Parameter {
-        /* parameter flags */
-        uint64_t flags;
-       
-        /* port link parameters */
-        struct PortLink {
-            uint8_t source;
-            uint8_t target;
-        } link;
-    } parameter;
-    
-    /* Available serial ports */
-    struct PortList {
-        uint8_t count;
-        TVSPPortListItem list[MAX_SERIAL_PORTS];
-    } ports;
-    
-    /* Available serial port links */
-    struct LinkList {
-        uint8_t count;
-        uint64_t list[MAX_PORT_LINKS];
-    } links;
-    
-} TVSPControllerData;
+#include <VSPController.hpp>
 
 namespace VSPClient {
-
-class VSPController;
 
 /* The classes below are not exported */
 #pragma GCC visibility push(hidden)
