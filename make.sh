0f63d6163cd33a12799b8d0049f642e59d1be80a
B. Eschrich <bjoern.eschrich@gmail.com>
2025-02-24 08:14:54 +0100

    build script updated

--- make.sh
+++ make.sh
@@ -3,23 +3,11 @@
 prjroot=`pwd`
 prjname="VSPDriver"
 
-# create build directory with ORSSerial modulemap
-if [ ! -d ${prjroot}/build/GeneratedModuleMaps ]; then
-    mkdir -p ${prjroot}/build/GeneratedModuleMaps
-fi
- 
-cd ${prjroot}/build/GeneratedModuleMaps
-
 basedir="${HOME}/Library/Developer/Xcode/DerivedData"
-orspath="/SourcePackages/checkouts/ORSSerialPort/build/GeneratedModuleMaps"
 prjpath=`basename ${basedir}/${prjname}-*`
 
 echo ":> ${basedir}"
 echo ":> ${prjpath}"
-mappath="${basedir}/${prjpath}${orspath}"
-ln -fs ${mappath}/ORSSerial.modulemap
-
-cd ${prjroot}
 
 echo ":> Build project..."
 
