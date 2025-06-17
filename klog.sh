2b2a2dc4eb77c141a9e62411fdea5ee34b72c53b
B. Eschrich <bjoern.eschrich@gmail.com>
2025-06-17 22:12:36 +0200

    klog script removed

--- klog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-## Watch VSPDriver activity by log stream
-#filter="'VSPDriver|VSPUserClient|VSPSerialPort'"
-filter="grep VSP"
-sudo log stream --level debug --color always --style compact \
-  --predicate 'process == "kernel"' | ${filter}
-#'VSPDriver|VSPUserClient|VSPSerialPort'
