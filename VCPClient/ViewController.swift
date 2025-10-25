544ba073d8aba4c9ec70ff1d82efbe79102aa08e
EoF Software Labs <lab@eofsl.com>
2025-10-25 21:41:48 +0200

    Initial Commit

---
+++ VCPClient/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VCPClient
+//
+//  Created by Björn Eschrich on 25.10.25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
